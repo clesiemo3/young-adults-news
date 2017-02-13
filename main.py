3a80f1659b6d7a0b1d4792e1f7c9a810f9993eb0
clesiemo3 <clesiemo3@gmail.com>
2017-02-12 19:57:11 -0600

    working scrape and post

---
+++ main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 12 18:50:47 2017
+
+@author: clesiemo3
+"""
+
+import feedparser
+import facebook
+import config
+import html
+import re
+from datetime import datetime as dt
+
+# efree
+feed = feedparser.parse('http://www.efree.org/events/feed/')
+
+message = "Weekly Announcements\nPosted by Bot - Report issues in comments\n"
+
+for x in feed.entries:
+    message += "\n" + x.title
+    message += "\n" + x.link
+    message += "\n" + dt.strptime(x.published, '%a, %d %b %Y %H:%M:%S %z').strftime('%A %B %d %H:%M %p')
+    summary = re.sub(r' <a href.+more.+$', '... [truncated]', x.summary)
+    message += "\n" + summary + "\n"
+
+message = html.unescape(message)
+
+# facebook
+graph = facebook.GraphAPI(config.user_token)
+groups = graph.get_object("me/groups")
+
+group_id = [x for x in groups['data'] if x['name'] == "yalt-test"][0]['id']
+graph.put_wall_post(message = message, profile_id = group_id)
+
+print(message)
