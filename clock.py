f6e1745d6eec4f0c4eb3bcf8ae6f241abfeafd61
clesiemo3 <clesiemo3@gmail.com>
2018-01-01 14:58:27 -0600

    trigger type

--- clock.py
+++ clock.py
@@ -8,10 +8,10 @@ schedule = BlockingScheduler()
 def tmp_print():
     print('testing...')
 
-schedule.add_job(tmp_print, minutes=2)
-schedule.add_job(news.main, minutes=5)
+schedule.add_job(tmp_print, trigger='interval', minutes=2)
+schedule.add_job(news.main, trigger='interval', minutes=5)
 
-schedule.add_job(auth_flow.update_token, days=2)
-schedule.add_job(news.main, day_of_week='wed', hour=19)
+schedule.add_job(auth_flow.update_token, trigger='interval', days=2)
+schedule.add_job(news.main, trigger='cron', day_of_week='wed', hour=19)
 
 schedule.start()
