7185fa3d2337649f0c5ca860567d45e7941cd799
clesiemo3 <clesiemo3@gmail.com>
2018-04-01 21:29:25 -0500

    wednesday at noon

--- clock.py
+++ clock.py
@@ -7,6 +7,6 @@ schedule = BlockingScheduler()
 schedule.add_job(auth_flow.update_token, trigger='cron', day_of_week='tue', hour=18)
 schedule.add_job(auth_flow.update_token, trigger='cron', day_of_week='thu', hour=18)
 schedule.add_job(auth_flow.update_token, trigger='cron', day_of_week='sat', hour=18)
-schedule.add_job(news.main, trigger='cron', day_of_week='sat', hour=22)
+schedule.add_job(news.main, trigger='cron', day_of_week='wed', hour=12)
 
 schedule.start()
